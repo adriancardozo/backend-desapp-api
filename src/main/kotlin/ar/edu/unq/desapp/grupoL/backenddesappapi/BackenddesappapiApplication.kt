e64e23d1da4a92d036b24fd95059211789bec1ea
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-10-04 18:08:13 -0300

    Se creó la estructura del proyecto

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoL/backenddesappapi/BackenddesappapiApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.desapp.grupoL.backenddesappapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackenddesappapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackenddesappapiApplication>(*args)
+}
