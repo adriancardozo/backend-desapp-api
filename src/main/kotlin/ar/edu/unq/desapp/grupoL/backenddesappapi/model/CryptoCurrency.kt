8e7c8b70f6b967c05cf9d62f6ef7bb2f7fa5cd83
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-10-05 02:05:50 -0300

    Se agregaron los criptoactivos al modelo

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoL/backenddesappapi/model/CryptoCurrency.kt
@@ -0,0 +1,5 @@
+package ar.edu.unq.desapp.grupoL.backenddesappapi.model
+
+import java.time.LocalDateTime
+
+data class CryptoCurrency(var name: String, var arPrice: Double, var quotationHour: LocalDateTime)
