96e090f94d33634b7d632b3675586466dd79e67a
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-10-12 02:00:31 -0300

    Se agregaron tests de CryptoCurrencyService

--- src/main/kotlin/ar/edu/unq/desapp/grupoL/backenddesappapi/services/CryptoCurrencyService.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoL/backenddesappapi/services/CryptoCurrencyService.kt
@@ -3,44 +3,38 @@ package ar.edu.unq.desapp.grupoL.backenddesappapi.services
 import ar.edu.unq.desapp.grupoL.backenddesappapi.model.CryptoCurrency
 import ar.edu.unq.desapp.grupoL.backenddesappapi.services.dtos.CryptoCurrencyDTO
 import ar.edu.unq.desapp.grupoL.backenddesappapi.services.dtos.DollarDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 import java.time.LocalDateTime
 
 @Service
 class CryptoCurrencyService {
-
     val cryptoCurrenciesSearched: Set<String> = setOf(
-        "ALICEUSDT",
-        "MATICUSDT",
-        "AXSUSDT",
-        "AAVEUSDT",
-        "ATOMUSDT",
-        "NEOUSDT",
-        "DOTUSDT",
-        "ETHUSDT",
-        "CAKEUSDT",
-        "BTCUSDT",
-        "BNBUSDT",
-        "ADAUSDT",
-        "TRXUSDT",
-        "AUDIOUSDT"
+        "ALICEUSDT", "MATICUSDT", "AXSUSDT", "AAVEUSDT",
+        "ATOMUSDT", "NEOUSDT", "DOTUSDT", "ETHUSDT",
+        "CAKEUSDT", "BTCUSDT", "BNBUSDT", "ADAUSDT",
+        "TRXUSDT", "AUDIOUSDT"
     )
-
     private val cryptoCurrencyApiEndPoint = "https://api1.binance.com/api/v3/ticker/price"
     private val dollarApiEndPoint = "https://api.estadisticasbcra.com/usd_of"
+    @Autowired
+    private lateinit var restTemplate: RestTemplate
 
     fun cryptoCurrencies() : List<CryptoCurrency> {
         return allCryptoCurrencies().filter { cryptoCurrenciesSearched.contains(it.name) }
     }
 
     fun allCryptoCurrencies(): List<CryptoCurrency> {
-        val response = RestTemplate().exchange(
+        val response = restTemplate.exchange(
             cryptoCurrencyApiEndPoint,
             HttpMethod.GET,
             null,
@@ -58,12 +52,17 @@ class CryptoCurrencyService {
         headers.contentType = MediaType.APPLICATION_JSON
         headers.set("Authorization", "BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjU0MzcxMjAsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJjYXJkb3pvYWRyaWFuOTk3QGdtYWlsLmNvbSJ9.eKm7XlvnqTNlx_0WNYPsENWSu1mDidgtCxtdpzEGdZlqarZdeRQauavp0QD_hTETgxcROUpljHgtmY2ZBMf2Ag")
         val requestEntity = HttpEntity("parameters", headers)
-        val response = RestTemplate().exchange(
+        val response = restTemplate.exchange(
             dollarApiEndPoint,
             HttpMethod.GET,
             requestEntity,
             object : ParameterizedTypeReference<List<DollarDTO>>() {}
         )
         return response.body?.last()?.v!!
+    }
+
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder.build()
     }
 }
---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoL/backenddesappapi/CryptoCurrencyServiceTests.kt
@@ -0,0 +1,84 @@
+package ar.edu.unq.desapp.grupoL.backenddesappapi
+
+import ar.edu.unq.desapp.grupoL.backenddesappapi.model.CryptoCurrency
+import ar.edu.unq.desapp.grupoL.backenddesappapi.services.CryptoCurrencyService
+import ar.edu.unq.desapp.grupoL.backenddesappapi.services.dtos.CryptoCurrencyDTO
+import ar.edu.unq.desapp.grupoL.backenddesappapi.services.dtos.DollarDTO
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+import org.mockito.exceptions.misusing.InvalidUseOfMatchersException
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.*
+import org.springframework.test.context.event.annotation.BeforeTestMethod
+import org.springframework.web.client.RestTemplate
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension::class)
+class CryptoCurrencyServiceTests {
+    @Mock
+    private lateinit var restTemplate: RestTemplate
+    @Autowired
+    @InjectMocks
+    private lateinit var cryptoCurrencyService: CryptoCurrencyService
+
+    @BeforeEach
+    fun beforeTest(){
+        `when`(restTemplate.exchange(anyString(), any(), any(), any(ParameterizedTypeReference::class.java)))
+            .thenReturn(aResponseEntity(someCryptoCurrencyDTOs()), aResponseEntity(someDollarDTOs()))
+    }
+
+    @Test
+    fun allCryptoCurrenciesRequestAreDelegatedToExternalWebService() {
+        val cryptoCurrenciesResult = cryptoCurrencyService.allCryptoCurrencies()
+        assertEquals(someCryptoCurrencyDTOs(), toCryptoCurrencyDTOs(cryptoCurrenciesResult))
+    }
+
+    @Test
+    fun cryptoCurrenciesRequestAreDelegatedToExternalWebService() {
+        val cryptoCurrenciesResult = cryptoCurrencyService.cryptoCurrencies()
+        assertEquals(cryptoCurrencyDTOSSearched(), toCryptoCurrencyDTOs(cryptoCurrenciesResult))
+    }
+
+    private fun <T> aResponseEntity(body: T): ResponseEntity<T> {
+        return ResponseEntity(body, HttpStatus.OK)
+    }
+
+    private fun toCryptoCurrencyDTOs(cryptoCurrencies: List<CryptoCurrency>): List<CryptoCurrencyDTO> {
+        return cryptoCurrencies.map { CryptoCurrencyDTO(it.name, it.arPrice / dollarQuotation()) }
+    }
+
+    private fun dollarQuotation(): Double {
+        return someDollarDTOs().last().v
+    }
+
+    private fun someCryptoCurrencyDTOs(): List<CryptoCurrencyDTO> {
+        return listOf(CryptoCurrencyDTO("CRYPTO",500.0)) + cryptoCurrencyDTOSSearched()
+    }
+
+    private fun someDollarDTOs(): List<DollarDTO> {
+        return listOf(DollarDTO("",100.0))
+    }
+
+    private fun cryptoCurrencyDTOSSearched(): List<CryptoCurrencyDTO> {
+        return cryptoCurrencyNamesSearched().map { CryptoCurrencyDTO(it, 200.0) }
+    }
+
+    private fun cryptoCurrencyNamesSearched(): Set<String> {
+        return setOf(
+            "ALICEUSDT", "MATICUSDT", "AXSUSDT", "AAVEUSDT",
+            "ATOMUSDT", "NEOUSDT", "DOTUSDT", "ETHUSDT",
+            "CAKEUSDT", "BTCUSDT", "BNBUSDT", "ADAUSDT",
+            "TRXUSDT", "AUDIOUSDT"
+        )
+    }
+}
